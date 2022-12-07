ba2847c25c93c8d647eba1da8cdcfb449402aa93
Sona-Martin <sonarosemartin@gmail.com>
2022-12-08 00:24:19 +0530

    created navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import './Navbar.css'
+import R_logo from '../../assets/r_logo.svg'
+
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar </h1>
+    <div className='Navbar_container'>
+
+        <div className='Navbar_logo'>
+            <img src={R_logo} alt='RessQ Logo' className='R_logo'/>
+        </div>
+
+        <div className='Navbar_items'>
+            <div>Home</div>
+            <div>Drives</div>
+            <div>About</div>
+            <div>Contact Us</div>
+        </div>
+
+        <div className='Navbar_auth'>
+            <div className='Navbar_auth_login'>
+                <button>Login</button>
+            </div>
+
+            <div className='Navbar_auth_signup'>
+                <button>Signup</button>
+            </div>
+        </div>
+
     </div>
   )
 }

59c78f32e093cc6b757afd3df2c7c4913a7c5085
Lekshmi S <slekshmi3949@gmail.com>
2022-12-07 21:34:47 +0530

    Started Dono reg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import MainPage from './pages/MainPage/MainPage'
 import './App.css';
-
+import { DonorReg } from './pages';
+import { BrowserRouter,Route,Routes} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-    <MainPage/>  
+      <BrowserRouter>
+         <Routes>
+            <Route path='/'element={<MainPage/>}/> 
+            <Route path='/donor-reg'element={<DonorReg/>}/>    
+         </Routes>
+      </BrowserRouter> 
     </div>
   );
 }

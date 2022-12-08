638e6259a030a6adf7e34e301d46ee9dc1af38ca
Lekshmi S <slekshmi3949@gmail.com>
2022-12-08 14:39:31 +0530

    revisedlogin page

--- src/pages/UserLogin/UserLogin.js
+++ src/pages/UserLogin/UserLogin.js
@@ -1,43 +1,17 @@
 import React from 'react'
 import './UserLogin.css'
-import MainLayout from '../../components/MainLayout/MainLayout'
 const UserLogin = () => {
   return (
-    <MainLayout>
-    <>
-      <div className='userlogin__main'> 
-       <div classname='userlogin_form_container' style={{backgroundColor:'#D9D9D9'}}>
-       <div classname='login-box'> 
-        <div classname='login'>
-          <p>Login</p>
-          </div>
-        
-      <form action="">
-        <div classname="user-box1">
-              <input type="text" name="username" required="" placeholder='username'/>
-             {/* <label>Username</label> */}
-          </div>
-
-        <div classname="user-box2">
-            <input type="password" name="" required="" placeholder='password'/>
-            {/* <label>Password</label> */}
-        </div>
-
-        <div classname="user-box3">
-      <input type="phone" name="" required="" placeholder='phone'/>
-      {/* <label>Phone</label> */}
+    <div className='login__form_main'>
+      <div className="login_form__container"></div>
+      <form action="" className='login__form'>
+        <p className='login__header'>Login</p>
+        <input className='login__input' type="text" placeholder='user name' required/>
+        <input className='login__input' type="password" placeholder='password' required/>
+        <input className='login__input' type="text" placeholder='phone number' />
+        <button className='login__button'>Log in</button>
+      </form>
     </div>
-      <div classname='frame'>
-    <button classname='sign-button'>SignUp</button>
-    </div>
-        </form>
-        </div>
-       </div>
-         
-        </div>
-    </>
-    
-    </MainLayout>
   )
 }
 

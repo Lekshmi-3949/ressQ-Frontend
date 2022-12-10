0cac26eb2408acf6a0548bde010996b60b29b129
Lekshmi S <slekshmi3949@gmail.com>
2022-12-10 09:13:50 +0530

    Styles not over

--- src/pages/UserLogin/UserLogin.js
+++ src/pages/UserLogin/UserLogin.js
@@ -1,17 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './UserLogin.css'
 import MainLayout from '../../components/MainLayout/MainLayout'
+import baseUrl from '../../utils/Urls'
+import axiosInstance from '../../utils/axios'
+import {useNavigate} from 'react-router-dom'
 const UserLogin = () => {
+  const[email,setemail]=useState('')
+  const[password,setPassword]=useState('')
+ const navigate=useNavigate()
+
+
+  const handleLogin= async(e)=>{
+    e.preventDefault()
+    axiosInstance.post(`${baseUrl}/api/token/`,{
+        email: email,
+        password : password
+    }).then((res)=>{
+        console.log(res)
+        localStorage.setItem('access_token',res.data.access);
+        localStorage.setItem('refresh_token',res.data.refresh);
+        axiosInstance.defaults.headers['Authorization']= 'Bearer ' + localStorage.getItem('access_token');
+        if(res.status===200)
+        
+        navigate('/')
+    },(error)=>{
+        console.log(error)
+    })
+
+}
   return (
     <MainLayout>
     <div className='login__form_main'>
       <div className="login_form__container" >
-      <form action="" className='login__form' >
+      <form onSubmit={handleLogin} className='login__form' >
         <p className='login__header'>Login</p>
-        <input className='login__input' type="text" placeholder='user name' required/>
-        <input className='login__input' type="password" placeholder='password' required/>
-        <input className='login__input' type="text" placeholder='phone number' />
-        <button className='login__button'>Log in</button>
+        <input className='login__input' type="text" value={email} onChange={(e)=>{setemail(e.target.value)}} placeholder='email id' required/>
+        <input className='login__input' type="password"  value={password} onChange={(e)=>{setPassword(e.target.value)}} placeholder='password' required/>
+        
+        <button  type='submit ' className='login__button'>SignUp</button>
+         <div><p>Already have an account?</p><a href="/" className='signin'>sign in</a></div>
       </form>
       </div>
     </div>

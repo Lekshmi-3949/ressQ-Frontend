afe45a4f0425b78063405fc9889ee1a51a2bf725
Lekshmi S <slekshmi3949@gmail.com>
2022-12-10 18:37:02 +0530

    Log in page

--- src/pages/UserLoginR/UserLoginR.js
+++ src/pages/UserLoginR/UserLoginR.js
@@ -6,15 +6,15 @@ import baseUrl from '../../utils/Urls'
 import axiosInstance from '../../utils/axios'
 import {useNavigate} from 'react-router-dom'
 const UserLoginR = () => {
-  const[username,setusername]=useState('')
-  const[phonenumber,setphonenumber]=useState('')
+  const[email,setemail]=useState('')
+  const[password,setpassword]=useState('')
  const navigate=useNavigate()
 
  const handleLogin= async(e)=>{
   e.preventDefault()
   axiosInstance.post(`${baseUrl}/api/token/`,{
-      username: username,
-      phonenumber : phonenumber
+      email: email,
+      password : password
   }).then((res)=>{
       console.log(res)
       localStorage.setItem('access_token',res.data.access);
@@ -35,9 +35,9 @@ const UserLoginR = () => {
       <div className="loginR_form__container"></div>
       <form action="" onSubmit={handleLogin} className='loginR__form'>
         <p className='loginR__header'>Login</p>
-        <input className='loginR__input' type="text" value={username} onChange={(e)=>{setusername(e.target.value)}}placeholder='user name' required/>
+        <input className='loginR__input' type="text" value={email} onChange={(e)=>{setemail(e.target.value)}}placeholder='Email Id' required/>
       
-        <input className='loginR__input' type="number" value={phonenumber} onChange={(e)=>{setphonenumber(e.target.value)}} placeholder='phone number' />
+        <input className='loginR__input' type="password" value={password} onChange={(e)=>{setpassword(e.target.value)}} placeholder='password' />
         <button className='loginR__button'>Log in</button>
         {/* <p className="forgot-password text-right mt-2"> */}
             {/* Forgot <a href="#">password?</a> */}
